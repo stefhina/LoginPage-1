f28840062eaad1ae7b594940e127704cb076a9ed
Stefhina George <72295494+stefhina@users.noreply.github.com>
2022-03-25 14:10:29 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -3,29 +3,48 @@ import './App.css';
 import Login from './components/login';
 function App() {
   return [
+    <div class="nav">
+      <nav class="navbar">
+        <ul>
+          <li>
+            <a href="#home">HOME</a>
+          </li>
+          <li>
+            <a href="#about">ABOUT US</a>
+          </li>
+          <li>
+            <a href="#contact">DOCS</a>
+          </li>
+        </ul>
+      </nav>
+    </div>,
     <div class="main">
-      <h1 class="headings">BAAP<br/></h1>
-      </div>,
-      <div class="side">
-      <h2>Backend As A Platfrom</h2>
-      </div>,
-      <div class="paragraph">
+      <h1 class="headings">
+        BaaP
+        <br />
+      </h1>
+    </div>,
+    <div class="side">
+      <h2>Backend as a Platfrom</h2>
+    </div>,
+    <div class="paragraph">
       <div class="text"></div>
       <ul class="option">
-      <li><span> A Low-code application development
-platform specifically for back-ends based on the technologies Node.js, Express, REST, GraphQL and MongoDB.</span></li>
+        <li>
+          <span>
+            {' '}
+            A Low-code application development platform specifically for
+            back-ends based on the technologies Node.js, Express, REST and
+            MongoDB.
+          </span>
+        </li>
       </ul>
-      </div>,
+    </div>,
 
     <div className="App">
       <Login />
-      
-    </div>
-    
-
-    
-    
-  ]
+    </div>,
+  ];
 }
 
 export default App;
---
+++ src/components/Signup.js
@@ -0,0 +1,87 @@
+import React from 'react'
+import { Grid,Paper, Avatar, TextField, Button, Typography,Link } from '@material-ui/core'
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import google from '../google.svg';
+import github from '../github.svg';
+const Signup=()=>{
+
+    const paperStyle={padding :20,height:'70vh',width:400, margin:"20px auto"}
+    const avatarStyle={backgroundColor:'#1bbd7e'}
+    const btnstyle={margin:'8px 0'}
+    return(
+        
+        <Grid>
+            <Paper elevation={10} style={paperStyle}>
+                <Grid align='center'>
+                     <Avatar style={avatarStyle}><LockOutlinedIcon/></Avatar>
+                    <h2>Sign Up</h2>
+                </Grid>
+                <TextField label='Firstname' placeholder='Enter Firstname' style={{
+        borderRadius: 5,
+        
+        padding: "10px 0px",
+        
+    }} fullWidth required/>
+    <TextField label='Lastname' placeholder='Enter Lastname' style={{
+        borderRadius: 5,
+        
+        padding: "10px 0px",
+        
+    }} fullWidth required/>
+                <TextField label='Password' placeholder='Enter password' type='password' style={{
+        borderRadius: 5,
+        
+        padding: "5px 0px",
+        
+    }} fullWidth required/>
+               
+                 
+                <Button type='submit' color='primary' variant="contained" style={{
+        borderRadius: 5,
+        backgroundColor: "#1A875B",
+        padding: "18px 36px",
+        
+    }} fullWidth>Sign in</Button>
+                <Typography style={{
+        borderRadius: 5,
+        
+        padding: "16px 5px",
+        
+    }} >
+                    
+                </Typography>
+                <Typography > Already have an account ?
+                     <Link href="#" >
+                        Sign In 
+                </Link>
+                </Typography>
+                
+                 <div>
+                 <img src={google} width="40" alt="Google" style={{
+        borderRadius: 5,
+        
+        padding: "16px 70px",
+        
+    }}/>
+                  <img src={github} width="28" alt="GitHub" style={{
+        borderRadius: 5,
+        
+        padding: "22px  0px",
+        
+    }}/>
+                 </div>
+
+            </Paper>
+        </Grid>
+        <ul>
+        <li><a href="default.asp">Home</a></li>
+        <li><a href="news.asp">News</a></li>
+        <li><a href="contact.asp">Contact</a></li>
+        <li><a href="about.asp">About</a></li>
+      </ul>
+    )
+}
+
+export default Signup
